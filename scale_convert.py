cc89c7e490140812977362444d43bb301df6889c
Ralph <ralphstevendc@gmail.com>
2020-06-08 15:41:23 -0700

    added my own path and also rotated the images

---
+++ scale_convert.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+
+import os
+from PIL import Image
+
+directory = os.path.abspath('/Users/ralphdelacruz/Desktop/Projects/PILProject/images')
+save_directory = os.path.abspath('/Users/ralphdelacruz/Desktop/Projects/PILProject/opts/icons')
+
+def main():
+    for file_name in os.listdir(directory):
+        output = os.path.splitext(file_name)[0]
+        new_path = os.path.join(directory, output)
+        im = Image.open(new_path)
+        rgb_im = im.convert('RGB')
+
+        new_path = os.path.join(save_directory, output)
+        rgb_im.rotate(270).resize((128,128)).save(new_path, 'JPEG')
+        print(output)
+main()
+
+
